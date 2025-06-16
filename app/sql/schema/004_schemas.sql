f31b67155600b0b50b438c43a2748edd0390b2c0
Nick.Abson <4947488+nickabs@users.noreply.github.com>
2025-06-16 09:27:07 +0100

    update the db migration so it deletes existing signal_types

--- app/sql/schema/004_schemas.sql
+++ app/sql/schema/004_schemas.sql
@@ -1,12 +1,15 @@
 -- +goose Up
 
 -- mark existing signal_types as 'skip validation'
-ALTER TABLE signal_types ADD COLUMN schema_content TEXT NOT NULL;
-
-UPDATE signal_types 
-SET (schema_url, schema_content) = ('https://github.com/skip/validation/main/schema.json', '{}');
+ALTER TABLE signal_types ADD COLUMN schema_content TEXT NOT NULL DEFAULT '{}';
 
 
+-- sadly this won't work in every case
+--UPDATE signal_types 
+--SET (schema_url, schema_content) = ('https://github.com/skip/validation/main/schema.json', '{}');
+
+-- so...
+DELETE FROM signal_types CASCADE;
 
 -- to keep things simple, JSON schema validation behaviour is now decided when setting up the signal type - if validation is used it is done at the point signals are created.
 ALTER TABLE signal_versions DROP CONSTRAINT validation_status_check;
@@ -14,4 +17,4 @@ ALTER TABLE signal_versions DROP CONSTRAINT validation_status_check;
 -- +goose Down
 ALTER TABLE signal_types DROP COLUMN schema_content;
 
-ALTER TABLE signal_versions ADD CONSTRAINT validation_status_check CHECK (validation_status IN ('pending','valid', 'invalid', 'n/a'))
+ALTER TABLE signal_versions ADD CONSTRAINT validation_status_check CHECK (validation_status IN ('pending','valid', 'invalid', 'n/a'));
